1e661f35bc0079f1868ea0a4fcd2fdf4ab369f33
abailey82@gmail.com <abailey82@gmail.com>
2019-11-07 22:31:52 +0000

    updates to spark 2.4.4 and hadoop 3.1.3

--- spark/start-master.sh
+++ spark/start-master.sh
@@ -5,4 +5,4 @@
 /spark/bin/spark-class org.apache.spark.deploy.master.Master \
     --host $SPARK_LOCAL_IP \
     --port $SPARK_MASTER_PORT \
-    --webui-port $SPARK_MASTER_WEBUI_PORT
+    --webui-port $SPARK_MASTER_WEBUI_PORT 
--- spark/start-worker.sh
+++ spark/start-worker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 /spark/bin/spark-class org.apache.spark.deploy.worker.Worker \
-    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER
+    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER \
+    --cores 1
