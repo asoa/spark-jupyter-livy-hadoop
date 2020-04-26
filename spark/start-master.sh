142ead589347098f0ce8b9f27c289ed4c869a870
asoa <asoa@Alexanders-Mac-mini.local>
2020-04-26 16:53:56 -0400

    env var update

--- spark/start-master.sh
+++ spark/start-master.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-/livy/bin/livy-server &
-
 /spark/bin/spark-class org.apache.spark.deploy.master.Master \
     --host $SPARK_LOCAL_IP \
     --port $SPARK_MASTER_PORT \
-    --webui-port $SPARK_MASTER_WEBUI_PORT 
+    --webui-port $SPARK_MASTER_WEBUI_PORT
